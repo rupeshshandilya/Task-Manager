7b5534ebb59ddce2e63d40f0f25b945b1df529b7
Rupesh Shandilya <rupeshshandilya@Rupeshs-MacBook-Pro.local>
2024-11-10 07:26:23 +0530

    changed authOption route

--- app/api/todos/new/route.ts
+++ app/api/todos/new/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import prisma from '@/lib/prisma';
 import { getServerSession } from 'next-auth/next';
-import { authOptions } from '../../auth/[...nextauth]/route';
+import { authOptions } from '@/lib/auth/authOptions';
 
 export async function POST(request: NextRequest) {
   const session = await getServerSession(authOptions);
